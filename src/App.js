2f5a5e96684a5da35ba5ecb41d222fff2c552592
GevorAbrahamyan1991 <abgevor@gmail.com>
2023-10-17 19:43:22 +0400

    Add Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "./pages/components/Header";
+import Footer from "./pages/components/Footer";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
