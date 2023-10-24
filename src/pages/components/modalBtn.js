912ce8554e86c00bf1a0ad5bc798b2c6aabf1f78
Vachagan <113465348+Metax7@users.noreply.github.com>
2023-10-24 22:45:57 +0400

    Adding modal window

--- src/pages/components/modalBtn.js
+++ src/pages/components/modalBtn.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./modal";
 
 const ModalBtn = () => {
+  const [modalActive, setModalActive] = useState();
+
   return (
     <>
-      <button className="bg-blue-500 py-1 px-3 font-bold text-sm text-white rounded-md">
+      <button
+        className="bg-blue-500 py-1 px-3 font-bold text-sm text-white rounded-md"
+        onClick={() => setModalActive(true)}
+      >
         SEE MORE
       </button>
+
+      <Modal active={modalActive} setActive={setModalActive}>
+        <div>
+            <p>
+                
+            </p>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </Modal>
     </>
   );
 };
